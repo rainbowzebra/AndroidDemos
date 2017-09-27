289cf80e9d5a2c361f2963de48bac12ca53e797b
lf <lfdfhl@gmail.com>
2017-09-27 15:42:56 +0800

    TextView

---
+++ TextView01/src/main/java/com/cn/textview01/MainActivity.java
@@ -0,0 +1,31 @@
+package com.cn.textview01;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private Context mContext;
+    private TextView mTextView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        init();
+    }
+
+    private void init(){
+        mContext=this;
+        mTextView= (TextView) findViewById(R.id.textView);
+        mTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(mContext,"点击了TextView",Toast.LENGTH_SHORT).show();
+            }
+        });
+        mTextView.setText("Hello Android");
+    }
+}
