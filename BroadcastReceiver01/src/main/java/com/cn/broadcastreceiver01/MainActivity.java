779ea0a8da2d199df7ab6163e349f66a79c13963
lf <lfdfhl@gmail.com>
2017-10-01 10:27:42 +0800

    2017年10月1日10:27:35

--- BroadcastReceiver01/src/main/java/com/cn/broadcastreceiver01/MainActivity.java
+++ BroadcastReceiver01/src/main/java/com/cn/broadcastreceiver01/MainActivity.java
@@ -1,10 +1,11 @@
 package com.cn.broadcastreceiver01;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Context mContext;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
