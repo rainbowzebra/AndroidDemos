cbaac98d97c119005a4e594bd7e95050bedb7f1c
lf <lfdfhl@gmail.com>
2017-10-01 18:21:07 +0800

    2017年10月1日18:20:55

--- Handler01/src/main/java/com/cn/handler01/MainActivity.java
+++ Handler01/src/main/java/com/cn/handler01/MainActivity.java
@@ -1,13 +1,49 @@
 package com.cn.handler01;
 
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private TextView mTextView;
+    private Handler mHandler;
+    public final static int DOWNLOAD_FINISH=9527;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+    }
+
+    private void init(){
+        mTextView= (TextView) findViewById(R.id.textView);
+        mHandler=new Handler(){
+            @Override
+            public void handleMessage(Message msg) {
+                super.handleMessage(msg);
+                if(msg.what==DOWNLOAD_FINISH){
+                    mTextView.setText("下载完成");
+                }
+            }
+        };
+
+        downLoad();
+    }
+
+    private void downLoad(){
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try{
+                    Thread.sleep(1000*8);
+                    Message message=new Message();
+                    message.what=DOWNLOAD_FINISH;
+                    mHandler.sendMessage(message);
+                }catch (Exception e){
+                }
+            }
+        }).start();
     }
 }
--- Handler02/src/main/java/com/cn/handler02/MainActivity.java
+++ Handler02/src/main/java/com/cn/handler02/MainActivity.java
@@ -1,13 +1,52 @@
 package com.cn.handler02;
 
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private TextView mTextView;
+    private Handler mHandler;
+    public final static int FINISH_ACTIVITY=9527;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+    }
+
+    private void init(){
+        mTextView= (TextView) findViewById(R.id.textView);
+        mHandler=new Handler(){
+            @Override
+            public void handleMessage(Message msg) {
+                super.handleMessage(msg);
+                if(msg.what==FINISH_ACTIVITY){
+                    finish();
+                }
+            }
+        };
+
+        testPostDelayed();
+    }
+
+    private void testPostDelayed(){
+
+        Runnable runnable=new Runnable() {
+            @Override
+            public void run() {
+                try{
+                    Message message=new Message();
+                    message.what=FINISH_ACTIVITY;
+                    mHandler.sendMessage(message);
+                }catch (Exception e){
+                }
+            }
+        };
+
+        mHandler.postDelayed(runnable,1000*4);
+        //mHandler.removeCallbacks(runnable);
     }
 }
